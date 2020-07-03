7409de95e5b164bac3afc17d5020052969626e03
surendra477 <58383796+surendra477@users.noreply.github.com>
2020-07-04 00:43:24 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import emojipedia from "../emojipedia";
+import Card from "./Card";
+var Year = new Date();
+var currentYear = Year.getFullYear();
+function createCard(emojipedia) {
+  return (
+    <Card
+      key={emojipedia.id}
+      emoji={emojipedia.emoji}
+      name={emojipedia.name}
+      meaning={emojipedia.meaning}
+    />
+  );
+}
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">{emojipedia.map(createCard)}</dl>
+      <div>
+        <footer className="footers">
+          Copyright @{currentYear}{" "}
+          <b>
+            <i>Surendra Ediga</i>
+          </b>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
